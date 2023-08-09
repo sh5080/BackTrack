203d72da499ae43017d2b3b185636de4286d8320
김승환 <prpn97@gmail.com>
2023-08-10 00:25:06 +0900

    Fix: 환경변수 추가 세팅

--- src/config/index.ts
+++ src/config/index.ts
@@ -27,6 +27,17 @@ const config: Config = {
   /** [포트] 포트 번호 */
   port: getEnvNumber("PORT"),
 
+  jwt: {
+    /** [JWT] 액세스 토큰 비밀 키 */
+    ACCESS_TOKEN_SECRET: getEnv("ACCESS_TOKEN_SECRET"),
+    /** [JWT] 리프레시 토큰 비밀 키 */
+    REFRESH_TOKEN_SECRET: getEnv("REFRESH_TOKEN_SECRET"),
+    /** [JWT] 리프레시 토큰 만료 시간 */
+    REFRESH_TOKEN_EXPIRES_IN: getEnv("REFRESH_TOKEN_EXPIRES_IN", "7d"),
+    /** [JWT] 액세스 토큰 만료 시간 */
+    ACCESS_TOKEN_EXPIRES_IN: getEnv("ACCESS_TOKEN_EXPIRES_IN", "12h"),
+  },
+
   database: {
     /** [데이터베이스] 데이터베이스 호스트 */
     DB_HOST: getEnv("DB_HOST"),
@@ -36,6 +47,11 @@ const config: Config = {
     DB_PASSWORD: getEnv("DB_PASSWORD"),
     /** [데이터베이스] 데이터베이스 이름 */
     DB_NAME: getEnv("DB_NAME"),
+  },
+
+  bcrypt: {
+    /** [bcrypt] 솔트 라운드 */
+    saltRounds: getEnvNumber("BCRYPT_SALT_ROUNDS", 10),
   },
 };
 
