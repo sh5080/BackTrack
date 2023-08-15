cf97f025f426e63b42a90bef18045ec976a313e9
김승환 <prpn97@gmail.com>
2023-08-15 20:41:05 +0900

    fix: 아이디찾기 / 비밀번호재설정 입력값 따라 별도로 반환

--- src/models/repositories/auth.repository.ts
+++ src/models/repositories/auth.repository.ts
@@ -29,9 +29,22 @@ export const AuthRepository = AppDataSource.getRepository(AuthEntity).extend({
 
     return existingUser;
   },
-  async findUserByEmail(email: string) {
-    const user = await AuthRepository.findOne({ where: { email } });
-    return user;
+  async findUser(username: string, email?: string) {
+    try {
+      if (email) {
+        const user = await this.findOne({ where: { username, email } });
+        if (user) {
+          return user;
+        } else {
+          throw Error;
+        }
+      } else {
+        const user = await this.findOne({ where: { email: username } });
+        return user;
+      }
+    } catch (error) {
+      throw error;
+    }
   },
 
   // async saveOauthUser(
