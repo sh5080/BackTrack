f688946e0fab42995a9cf23124c50a45d68c3acd
김승환 <prpn97@gmail.com>
2023-08-17 22:48:13 +0900

    feat: 마이페이지 분리

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -2,10 +2,13 @@ import express from "express";
 import backtrackRouter from "./backtrackRoutes";
 import authRouter from "./authRoutes";
 import oauthRouter from "./oauthRoutes";
+import mypageRouter from "./mypageRoutes";
 
 const router = express.Router();
 
 router.use("/backtrack", backtrackRouter);
 router.use("/auth", authRouter);
 router.use("/oauth", oauthRouter);
+
+router.use("/mypage", mypageRouter);
 export default router;
