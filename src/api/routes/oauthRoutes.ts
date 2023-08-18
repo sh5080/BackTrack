f55717ee95eab8885c784f3ecb9e3922ee23a26f
김승환 <prpn97@gmail.com>
2023-08-18 19:30:35 +0900

    chore: 주석제거

--- src/api/routes/oauthRoutes.ts
+++ src/api/routes/oauthRoutes.ts
@@ -4,12 +4,9 @@ import * as oauthController from "../../controllers/oauthController";
 const router = Router();
 
 // /** [인증] 카카오 콜백 */
-// router.get("/kakao", oauthController.kakaoCallback);
-router.get("/kakao/login", oauthController.kakaoLogin);
+router.get("/kakao/callback", oauthController.kakaoCallback);
 
 // /** [인증] 구글 콜백 */
 router.get("/google/callback", oauthController.googleCallback);
-
-router.get("/google/login", oauthController.googleLogin);
 
 export default router;
