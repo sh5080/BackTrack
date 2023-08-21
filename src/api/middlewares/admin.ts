5584362b070a9e9e72c2f3e4ace7bafff5525051
김승환 <prpn97@gmail.com>
2023-08-21 19:49:31 +0900

    fix: 에러처리 try-catch로 수정

--- src/api/middlewares/admin.ts
+++ src/api/middlewares/admin.ts
@@ -1,9 +1,22 @@
-import { Response, NextFunction } from 'express';
-import { CustomRequest } from '../../types/customRequest';
+import { Response, NextFunction } from "express";
+import { CustomRequest } from "../../types/customRequest";
 
-export const ensureAdmin = (req: CustomRequest, res: Response, next: NextFunction) => {
-  if (req.user?.role !== 'ADMIN') {
-    return res.status(403).json({ message: '관리자 권한이 없습니다.' });
+export const ensureAdmin = async (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    if (req.user === undefined) {
+      throw Error;
+    }
+    const session = req.user;
+    if (session?.role !== "ADMIN") {
+      return res.status(403).json({ message: "관리자 권한이 없습니다." });
+    }
+    next();
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "서버 오류가 발생했습니다." });
   }
-  next();
 };
