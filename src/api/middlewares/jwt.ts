d9c240c48888591447656beefe593b669dee6d8b
김승환 <prpn97@gmail.com>
2023-08-26 15:32:46 +0900

    feat: 전역으로 jwt검증하는 로직 별도구현

--- src/api/middlewares/jwt.ts
+++ src/api/middlewares/jwt.ts
@@ -14,7 +14,6 @@ export const validateToken = async (
 ) => {
   let accessToken;
   let refreshToken;
-
   if (req.headers?.cookie) {
     const cookies = req.headers.cookie.split("; ");
     for (let i = 0; i < cookies.length; i++) {
@@ -83,7 +82,6 @@ export const validateToken = async (
           username: decodedRefreshToken.username,
           role: decodedRefreshToken.role,
         };
-
         res
           .cookie("accessToken", newAccessToken, {
             httpOnly: true,
@@ -122,30 +120,121 @@ export const validateToken = async (
   }
 };
 
-export const isLoggedIn = (
+export const isLoggedIn = async (
   req: CustomRequest,
   res: Response,
   next: NextFunction
 ) => {
-  console.log("로그인 유지 검증");
-  if (req.headers.cookie) {
-    const cookies = req.headers.cookie.split("; ");
-    let token = null;
+  let accessToken;
+  let refreshToken;
 
+  if (req.headers?.cookie) {
+    const cookies = req.headers.cookie.split("; ");
     for (let i = 0; i < cookies.length; i++) {
       if (cookies[i].startsWith("token=")) {
-        token = cookies[i].substring(6);
+        const cookieValue = cookies[i].substring(6);
+        const decodedValue = decodeURIComponent(cookieValue);
+        const jsonStr = decodedValue.substring(2);
+        const cookieObject = JSON.parse(jsonStr);
+        accessToken = cookieObject.accessToken;
+        refreshToken = cookieObject.refreshToken;
         break;
       }
     }
+  }
 
-    if (token) {
-      validateToken(req, res, next);
-      // res.status(200).send({ message: "유효한 토큰으로 인증됨" });
-    } else {
-      next();
-    }
-  } else {
+  if (!accessToken) {
+    return next(
+      new AppError(
+        CommonError.UNAUTHORIZED_ACCESS,
+        "접근 거부. 유효한 토큰을 제공해주세요.",
+        401
+      )
+    );
+  }
+
+  try {
+    req.user = jwt.verify(accessToken, ACCESS_TOKEN_SECRET) as JwtPayload & {
+      username: string;
+      role: string;
+    };
+
+    console.log(req.user);
+
+    // res.status(200).json(req.user.username);
+    res.status(200).json({ message: "검증완료" });
     next();
+  } catch (err) {
+    if (err instanceof jwt.TokenExpiredError) {
+      if (!refreshToken) {
+        console.error(err);
+        return next(
+          new AppError(
+            CommonError.TOKEN_EXPIRED_ERROR,
+            "토큰이 유효하지 않습니다. 토큰을 확인해주세요.",
+            401
+          )
+        );
+      }
+
+      try {
+        const decodedRefreshToken = jwt.verify(
+          refreshToken,
+          REFRESH_TOKEN_SECRET
+        ) as JwtPayload & {
+          username: string;
+          role: string;
+        };
+
+        const newAccessToken = jwt.sign(
+          {
+            username: decodedRefreshToken.username,
+            role: decodedRefreshToken.role,
+          },
+          ACCESS_TOKEN_SECRET,
+          {
+            expiresIn: ACCESS_TOKEN_EXPIRES_IN,
+          }
+        );
+
+        req.user = {
+          username: decodedRefreshToken.username,
+          role: decodedRefreshToken.role,
+        };
+        res
+          .cookie("accessToken", newAccessToken, {
+            httpOnly: true,
+            secure: true,
+          })
+          .status(200)
+          .json({ message: "새로운 엑세스 토큰이 발급되었습니다." });
+      } catch (err) {
+        if (err instanceof jwt.TokenExpiredError) {
+          res.clearCookie("refreshToken");
+          return next(
+            new AppError(
+              CommonError.TOKEN_EXPIRED_ERROR,
+              "리프레쉬 토큰이 만료되었습니다. 다시 로그인 해주세요.",
+              401
+            )
+          );
+        }
+        return next(
+          new AppError(
+            CommonError.TOKEN_EXPIRED_ERROR,
+            "토큰이 유효하지 않습니다. 토큰을 확인해주세요.",
+            401
+          )
+        );
+      }
+    } else {
+      return next(
+        new AppError(
+          CommonError.UNAUTHORIZED_ACCESS,
+          "토큰이 유효하지 않습니다. 토큰을 확인해주세요.",
+          401
+        )
+      );
+    }
   }
 };
