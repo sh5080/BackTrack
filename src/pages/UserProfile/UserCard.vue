8a692fa10a8f3431e9ac7514b6e739abd9133256
김승환 <prpn97@gmail.com>
2023-08-27 22:52:53 +0900

    feat: 닉네임 변경 1차완료

--- src/pages/UserProfile/UserCard.vue
+++ src/pages/UserProfile/UserCard.vue
@@ -5,62 +5,136 @@
       <div class="user-info">
         <div class="info-item">
           <span class="info-label">아이디</span>
-          <span class="info-value">{{ fetchedUserInfo.username }}</span>
+          <span class="info-value">{{ fetchedUserInfo.username }} </span>
+
+          <button
+            class="generate-button"
+            type="button"
+            @click="showFindPassword"
+          >
+            비밀번호 재설정
+          </button>
         </div>
+
         <div class="info-item">
           <span class="info-label">닉네임</span>
           <span class="info-value">{{ fetchedUserInfo.nickname }}</span>
+
+          <button
+            class="generate-button"
+            type="button"
+            @click="openNickname"
+            v-show="!nicknameExpanded"
+          >
+            닉네임 변경
+          </button>
+          <button
+            class="generate-button"
+            type="button"
+            @click="closeNickname"
+            v-show="nicknameExpanded"
+          >
+            취소
+          </button>
+        </div>
+        <div
+          class="change-nickname"
+          :style="{ height: nicknameExpanded ? '500px' : '0px' }"
+        >
+          <p
+            style="
+              font-size: 3.5em;
+              padding: 0.5em;
+              height: 1em;
+              margin-top: 100px;
+              margin-left: 0px;
+            "
+            v-show="nicknameExpanded"
+          >
+            - 길이는 최대 15자 이내로 작성해주세요. <br />- 이모티콘 및 일부
+            특수문자 사용 불가합니다. &&lt;>()'/"
+          </p>
+          <input
+            class="change-input"
+            type="text"
+            v-model="newNickname"
+            v-show="nicknameExpanded"
+            placeholder="닉네임 입력 (최대 15자)"
+          />
+          <button
+            class="generate-button"
+            type="button"
+            @click="changeNickname"
+            v-show="nicknameExpanded"
+            style="margin-left: 100px"
+          >
+            변경하기
+          </button>
         </div>
         <div class="info-item">
           <span class="info-label">이메일</span>
           <span class="info-value">{{ fetchedUserInfo.email }}</span>
+
+          <button
+            class="generate-button"
+            type="button"
+            @click="generateBacktrack"
+          >
+            이메일 변경
+          </button>
         </div>
         <div class="info-item">
           <span class="info-label">현재 로그인방식</span>
-          <span class="info-value">{{ $store.state.provider }}</span>
+          <span class="info-value">{{ this.provider }}</span>
         </div>
       </div>
     </div>
-
-    <div slot="footer" class="text-center d-flex justify-content-center">
-      <button href="#" class="btn btn-simple">
-        <i class="fa fa-facebook-square"></i>
-      </button>
-      <button href="#" class="btn btn-simple">
-        <i class="fa fa-twitter"></i>
-      </button>
-      <button href="#" class="btn btn-simple">
-        <i class="fa fa-google-plus-square"></i>
-      </button>
-    </div>
+    <v-dialog v-model="$store.state.showFindPasswordModal">
+      <FindPassword />
+    </v-dialog>
   </card>
 </template>
 <script>
 import Card from "src/components/Cards/Card.vue";
+import FindPassword from "../../components/LoginModal/findPasswordModal.vue";
 import axios from "axios";
 export default {
   props: ["userInfo"],
   components: {
     Card,
+    FindPassword,
   },
   data() {
     return {
       username: this.$store.state.loggedInUsername,
       fetchedUserInfo: null,
+      provider: "Backtrack",
+      nicknameExpanded: false,
     };
   },
   created() {
     this.fetchUserInfo();
   },
   methods: {
-    getClasses(index) {
-      var remainder = index % 3;
-      if (remainder === 0) {
-        return "col-md-3 col-md-offset-1";
-      } else if (remainder === 2) {
-        return "col-md-4";
-      } else {
-        return "col-md-3";
+    openNickname() {
+      this.nicknameExpanded = !this.nicknameExpanded;
+    },
+    closeNickname() {
+      this.nicknameExpanded = !this.nicknameExpanded;
+    },
+    async changeNickname() {
+      this.nicknameExpanded = !this.nicknameExpanded;
+      try {
+        const response = await axios.put(
+          `http://localhost:4000/api/mypage/userInfo`,
+          {
+            withCredentials: true,
+          }
+        );
+
+        this.fetchedUserInfo = response.data;
+      } catch (error) {
+        console.error("Failed to fetch user info:", error);
       }
     },
     async fetchUserInfo() {
@@ -72,16 +146,23 @@ export default {
             withCredentials: true,
           }
         );
-
+        this.provider = localStorage.getItem("oauth");
         this.fetchedUserInfo = response.data;
       } catch (error) {
         console.error("Failed to fetch user info:", error);
       }
     },
+    async showFindPassword() {
+      this.$store.commit("toggleFindPasswordModal", true);
+    },
   },
 };
 </script>
 <style scoped>
+.card-user {
+  overflow: hidden;
+  width: 2750px;
+}
 .author {
   margin-top: 20px;
   padding: 50px 100px;
@@ -94,25 +175,45 @@ export default {
 }
 
 .user-info {
-  border-top: 1px solid #ccc;
-  margin-top: 30px;
+  /* border-top: 1px solid #ccc; */
+  margin-top: 150px;
+  margin-bottom: 100px;
 }
 
 .info-item {
-  margin-top: 50px;
+  position: relative;
+  margin-top: 100px;
   margin-bottom: 10px;
+  border-top: 1px solid #ccc;
 }
 
 .info-label {
   display: inline-block;
   width: 600px;
-  font-size: 80px;
+  font-size: 75px;
   font-weight: 400;
+  margin-top: 70px;
 }
 
 .info-value {
   display: inline-block;
-  font-size: 80px;
+  font-size: 70px;
+  width: 1250px;
   margin-left: 10px;
+}
+.generate-button {
+  border: 1px solid #ccc;
+  padding: 20px 0px;
+  width: 600px;
+  display: inline-block;
+
+  font-size: 70px;
+}
+.change-input {
+  font-size: 70px;
+  border: 1px solid #ccc;
+  margin-top: 200px;
+  margin-left: 350px;
+  padding: 20px 200px;
 }
 </style>
