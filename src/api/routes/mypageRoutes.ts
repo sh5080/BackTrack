855a95e3b6fdcf5a7b800a33639355749dee1699
김승환 <prpn97@gmail.com>
2023-08-28 22:45:07 +0900

    fix: 회원정보수정 닉네임, 이메일 분리

--- src/api/routes/mypageRoutes.ts
+++ src/api/routes/mypageRoutes.ts
@@ -7,14 +7,18 @@ const router = Router();
 // /** [마이페이지] 회원정보 조회 */
 router.get("/userInfo", validateToken, mypageController.getUserInfo);
 
-// /** [마이페이지] 회원정보 수정 */
-router.patch("/userInfo", validateToken, mypageController.updateUserInfo);
-
-/** [사용자] 닉네임 재설정 */
+/** [사용자] 닉네임 변경 */
 router.put(
   "/userInfo/nickname",
   validateToken,
   validateRequestBody(["nickname"]),
+  mypageController.updateUserInfo
+);
+/** [사용자] 닉네임 재설정 */
+router.put(
+  "/userInfo/email",
+  validateToken,
+  validateRequestBody(["email"]),
   mypageController.updateUserInfo
 );
 
