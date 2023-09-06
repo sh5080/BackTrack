75c86fcd21948a177f35d1d97c51380dd56ce722
김승환 <prpn97@gmail.com>
2023-09-06 13:32:27 +0900

    feat: nickname, message 클라이언트와 통신, redis저장까지 확인

--- src/loaders/socketIoLoader.ts
+++ src/loaders/socketIoLoader.ts
@@ -1,6 +1,8 @@
 import { Server } from "socket.io";
 import { Application } from "express";
+import { saveChatToRedis, redisClient } from "../config/redis";
 
+const MESSAGE_COUNTER_KEY = "chat_id_counter";
 export function socketIoLoader(app: Application): Server {
   const httpServer = app.listen(3000);
   const io = new Server(httpServer, {
@@ -13,9 +15,26 @@ export function socketIoLoader(app: Application): Server {
   io.on("connection", (socket) => {
     console.log("SocketIO is connected");
 
-    socket.on("chat message", (message) => {
-      console.log(`Received message from client: ${message}`);
-      io.emit("chat message", message);
+    socket.on("chat", async (data) => {
+      const { nickname, message } = data;
+      console.log(`Received message from ${nickname}: ${message}`);
+
+      try {
+        const chatId = await redisClient.incr(MESSAGE_COUNTER_KEY);
+        const chatIdWithNickname = `${chatId}_${nickname}`;
+
+        await saveChatToRedis(chatIdWithNickname, message, 30 * 24 * 60 * 60); //30일
+        console.log(
+          "Redis saved successful ",
+          chatIdWithNickname,
+          ": ",
+          message
+        );
+      } catch (error) {
+        console.error("Error saving message to Redis:", error);
+      }
+
+      io.emit("chat", message);
     });
 
     socket.on("disconnect", () => {
