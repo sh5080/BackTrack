340cade5150ceed326ca74c4801357db2787c263
김승환 <prpn97@gmail.com>
2023-09-06 10:33:30 +0900

    feat: socket.io 세팅

--- src/main.js
+++ src/main.js
@@ -3,14 +3,15 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
-import { store } from "./routes/store";
+import { store } from "./store/index";
 import LightBootstrap from "./light-bootstrap-main";
 import { router } from "./routes/routes";
-
+import { SocketPlugin } from "./plugins/socketPlugin";
 import "./registerServiceWorker";
 
 loadFonts();
 const app = createApp(App);
+app.use(SocketPlugin);
 app.use(vuetify);
 app.use(router);
 app.use(store);
---
+++ src/plugins/socketPlugin.js
@@ -0,0 +1,31 @@
+// import Vue from "vue";
+import io from "socket.io-client";
+
+const socket = io("http://localhost:3000");
+console.log("과연: ", socket);
+export const SocketPlugin = {
+  install(app) {
+    app.provide("join", ($payload) => {
+      console.log("Joining with payload:", $payload);
+      socket.emit("join", $payload);
+    });
+
+    app.provide("leave", ($payload) => {
+      console.log("Leaving with payload:", $payload);
+      socket.emit("leave", $payload);
+    });
+
+    app.provide("sendMessage", ($payload) => {
+      console.log("Sending message with payload:", $payload);
+      socket.emit("chat", {
+        msg: $payload.msg,
+        name: $payload.name,
+        avatar: $payload.avatar,
+      });
+    });
+
+    app.provide("socket", socket);
+  },
+};
+
+// Vue.use(SocketPlugin);
