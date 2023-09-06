6d305bd4b25484b7273ba1f5336323e2751ad683
김승환 <prpn97@gmail.com>
2023-09-06 14:40:30 +0900

    chore: 주석제거

--- src/plugins/socketPlugin.js
+++ src/plugins/socketPlugin.js
@@ -1,26 +1,21 @@
-// import Vue from "vue";
 import io from "socket.io-client";
 
 const socket = io("http://localhost:3000");
-console.log("과연: ", socket);
+
 export const SocketPlugin = {
   install(app) {
     app.provide("join", ($payload) => {
-      console.log("Joining with payload:", $payload);
       socket.emit("join", $payload);
     });
 
     app.provide("leave", ($payload) => {
-      console.log("Leaving with payload:", $payload);
       socket.emit("leave", $payload);
     });
 
     app.provide("sendMessage", ($payload) => {
-      console.log("Sending message with payload:", $payload);
       socket.emit("chat", {
         message: $payload.message,
-        name: $payload.name,
-        avatar: $payload.avatar,
+        nickname: $payload.nickname,
       });
     });
 
