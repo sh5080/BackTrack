1cb366e0a95d4ad604377787473677394f9506e5
김승환 <prpn97@gmail.com>
2023-09-09 23:53:44 +0900

    fix: 만들어진 백킹트랙 db에서 username확인하여 전부 반환(paginate)

--- src/controllers/backtrackController.ts
+++ src/controllers/backtrackController.ts
@@ -34,26 +34,15 @@ export const getBacktrack = async (
   next: NextFunction
 ) => {
   try {
-    const fileName = req.query.filePath;
-    // const filePath = path.join(__dirname, "../..", "uploads", fileName);
-    console.log("여기: ", fileName);
-    fs.readFile(fileName, (err, data) => {
-      if (err) {
-        console.error("Error reading file:", err);
-        res.status(500).json({ error: "Failed to read file" });
-      } else {
-        res.setHeader(
-          "Content-Disposition",
-          `attachment; filename=${fileName}`
-        );
-        res.setHeader("Content-Type", "application/octet-stream");
-        res.send(data);
-      }
-    });
+    const username = req.user!.username;
+    const page = req.query.page;
+
+    const backtrackData = await backtrackService.getBacktrack(
+      username,
+      parseInt(page)
+    );
+    res.json({ message: "악보 불러오기가 완료되었습니다.", backtrackData });
   } catch (error) {
     next(error);
   }
 };
-
-// const synth = new Tone.Synth().toDestination();
-// synth.triggerAttackRelease("C4", "8n");
