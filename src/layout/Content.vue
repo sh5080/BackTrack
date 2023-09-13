7c581200ee21f979d05bcfa77a2ae1eee90f37c9
김승환 <prpn97@gmail.com>
2023-09-13 20:26:45 +0900

    feat: 싱글페이지렌더링 하기 위해서 라우터 가져옴

--- src/layout/Content.vue
+++ src/layout/Content.vue
@@ -1,13 +1,64 @@
 <template>
-  <router-view v-slot="{ Component }">
-    <transition>
-      <component :is="Component" />
-    </transition>
-  </router-view>
+  <div>
+    <div v-if="showComponent">
+      <!-- 라우터~~! -->
+      <router-view>
+        <component
+          :is="component"
+          v-for="component in currentComponents"
+          :key="component.name"
+        />
+      </router-view>
+    </div>
+    <div v-else>
+      <!--  메인 화면 등 -->
+    </div>
+  </div>
 </template>
+
 <script>
-export default {};
+import Main from "../pages/Main.vue";
+import Backtrack from "../pages/BacktrackGenerator.vue";
+import Questions from "../pages/Questions.vue";
+import Board from "../pages/Board.vue";
+export default {
+  computed: {
+    showComponent() {
+      return !!this.currentComponents;
+    },
+    currentComponents() {
+      const matchedChildren = this.currentRoute.matched[0].children;
+      const components = [];
+
+      for (const child of matchedChildren) {
+        if (child.name === "Main") {
+          components.push(Main);
+        } else if (child.name === "Backtrack") {
+          components.push(Backtrack);
+        } else if (child.name === "Questions") {
+          components.push(Questions);
+        } else if (child.name === "Board") {
+          components.push(Board);
+        }
+      }
+      console.log("1: ", components);
+
+      return components;
+    },
+  },
+  watch: {
+    $route(to, from) {
+      this.currentRoute = to;
+    },
+  },
+  data() {
+    return {
+      currentRoute: this.$route,
+    };
+  },
+};
 </script>
+
 <style>
 .fade-enter-active,
 .fade-leave-active {
