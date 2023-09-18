90214f4bf62a2dfe3b6f355467fe894dc23a93e8
김승환 <prpn97@gmail.com>
2023-09-18 21:56:52 +0900

    feat: post api

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,17 +1,19 @@
 import express from "express";
 import backtrackRouter from "./backtrackRoutes";
+import postRouter from "./postRoutes";
 import authRouter from "./authRoutes";
 import oauthRouter from "./oauthRoutes";
 import mypageRouter from "./mypageRoutes";
-import chatRouter from "./chatRoutes";
+// import chatRouter from "./chatRoutes";
 import adminRouter from "./adminRoutes";
 const router = express.Router();
 
 router.use("/backtrack", backtrackRouter);
+router.use("/post", postRouter);
 router.use("/auth", authRouter);
 router.use("/oauth", oauthRouter);
 router.use("/mypage", mypageRouter);
-router.use("/chat", chatRouter);
+// router.use("/chat", chatRouter);
 
 router.use("/admin", adminRouter);
 export default router;
