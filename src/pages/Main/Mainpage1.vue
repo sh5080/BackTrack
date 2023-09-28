6df34119132049856739ca679100947c9bb18776
김승환 <prpn97@gmail.com>
2023-09-28 18:55:52 +0900

    feat: backtrack으로 스크롤이동, url상관없이 강제이동

--- src/pages/Main/Mainpage1.vue
+++ src/pages/Main/Mainpage1.vue
@@ -1,31 +1,55 @@
 <template>
   <div class="main-body">
     <h1 class="main-title">원하는 코드대로 반주를 생성해보세요</h1>
+    <h1 class="main-title start" @click="goBacktrack">시작하기</h1>
   </div>
 </template>
-
 <script>
 export default {
-  // 컴포넌트 로직이나 데이터를 추가할 수 있습니다.
+  methods: {
+    goBacktrack() {
+      const currentPath = this.$route.path;
+
+      if (currentPath === "/backtrack") {
+        this.scrollToComponent();
+      } else {
+        // 다른 경로로 이동
+        this.$router.push("/backtrack");
+      }
+    },
+    scrollToComponent() {
+      const componentId = "Backtrack";
+      const componentData = document.querySelector(`#${componentId}`);
+
+      if (componentData) {
+        const yOffset = componentData.getBoundingClientRect().top;
+        window.scrollBy({ top: yOffset - 270, behavior: "smooth" });
+      }
+    },
+  },
 };
 </script>
 
 <style scoped>
+.main-body {
+  text-align: center;
+  position: relative;
+  top: 36%;
+}
+
 .main-title {
   color: #1d1d22;
   font-size: 10rem;
   line-height: 10rem;
-
   z-index: 999;
-  width: 3000px;
-  max-width: 3000px;
   text-align: center;
+  margin: 0;
+  padding: 80px;
+}
 
-  margin-left: 50px;
-  margin-right: 50px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+.start:hover {
+  transform: scale(1.1);
+  transition: transform 0.3s ease;
+  cursor: pointer;
 }
 </style>
