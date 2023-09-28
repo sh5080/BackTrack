9563db627727cd6cffa4f00e85de8add03ced4ac
김승환 <prpn97@gmail.com>
2023-09-28 19:53:02 +0900

    feat: socket.io 사용으로 인한 chat api제거

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -6,7 +6,6 @@ import authRouter from "./authRoutes";
 import oauthRouter from "./oauthRoutes";
 import mypageRouter from "./mypageRoutes";
 import commentRouter from "./commentRoutes";
-// import chatRouter from "./chatRoutes";
 import adminRouter from "./adminRoutes";
 const router = express.Router();
 
@@ -17,7 +16,6 @@ router.use("/backtrack", backtrackRouter);
 router.use("/post", postRouter);
 router.use("/community", communityRouter);
 router.use("/comment", commentRouter);
-// router.use("/chat", chatRouter);
 
 router.use("/admin", adminRouter);
 export default router;
