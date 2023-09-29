0e9e47474c2395764cb1731df300b3363fd9f05f
김승환 <prpn97@gmail.com>
2023-09-29 23:57:49 +0900

    feat: 버튼으로 테이블 동적 세팅

--- src/pages/Main/Mainpage3.vue
+++ src/pages/Main/Mainpage3.vue
@@ -1,36 +1,59 @@
 <template>
   <div class="table-container">
+    <div class="current-time">{{ formattedDate }}</div>
+    <div class="table-header-container">
+      <v-btn class="table-header" @click="showTable('currentFamousPost')"
+        >최근 일주일</v-btn
+      >
+      <v-btn class="table-header" @click="showTable('totalFamousPost')"
+        >전체 게시물</v-btn
+      >
+      <v-btn class="table-header" @click="showTable('totalFamousUser')"
+        >전체 사용자</v-btn
+      >
+    </div>
     <v-data-table
-      :headers="headers"
       :items="posts"
       :items-per-page="10"
       class="mx-auto table"
       style="
         font-size: 60px;
-        margin-top: 100px;
+        /* margin-top: 100px; */
         /* max-height: 1775px; */
         height: 1775px;
         width: 2500px;
+        align-content: center;
       "
     >
-      <template v-slot:top>
-        <v-toolbar flat>
-          <v-toolbar-title style="font-size: 5rem">실시간 랭킹</v-toolbar-title>
-        </v-toolbar>
-      </template>
-
       <template v-slot:item="item">
         <tr>
-          <td>{{ item.index + 1 }}</td>
           <td>
-            {{
-              item.item.selectable.likedUsers
-                ? item.item.selectable.likedUsers.length
-                : ""
-            }}
+            <v-row>
+              <v-col cols="1" class="rank-number">
+                <span class="table-rank">
+                  {{ item.index + 1 }}
+                </span>
+              </v-col>
+
+              <v-col cols="6" class="rank-left">
+                <span class="table-title">{{
+                  item.item.selectable.title
+                }}</span>
+                <span class="table-author">{{
+                  item.item.selectable.author
+                }}</span>
+              </v-col>
+              <v-col cols="5" class="rank-right">
+                <span class="table-likedCount">
+                  {{
+                    item.item.selectable.likedUsers
+                      ? item.item.selectable.likedUsers.length
+                      : ""
+                  }}
+                </span>
+              </v-col>
+            </v-row>
           </td>
-          <td>{{ item.item.selectable.title }}</td>
-          <td>{{ item.item.selectable.author }}</td>
         </tr>
       </template>
       <template v-slot:bottom></template>
@@ -44,16 +67,35 @@ export default {
     return {
       headers: [
         { title: "등수", key: "index+1", sortable: false },
-        { title: "누적 좋아요", key: "likedUsers.length", sortable: false },
-        { title: "제목", key: "title", sortable: false },
-        { title: "작성자", key: "author", sortable: false },
+        { title: "좋아요", key: "likedUsers.length", sortable: false },
       ],
       posts: [],
       currentPage: 1,
+      currentTime: new Date(),
     };
+  },
+  computed: {
+    formattedDate() {
+      const options = {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      };
+      return this.currentTime
+        .toLocaleDateString("ko-KR", options)
+        .replace(/\./g, "-")
+        .replace(/\s/g, " ")
+        .replace(/-/g, "");
+    },
   },
   created() {
     this.fetchPosts();
+    setInterval(() => {
+      this.currentTime = new Date();
+    }, 1000);
   },
   methods: {
     async fetchPosts() {
@@ -102,5 +144,40 @@ export default {
   overflow: hidden;
   width: 2000px;
   margin: 0 auto;
+}
+.current-time {
+  font-size: 6rem;
+  display: flex;
+  justify-content: center;
+  margin-top: 100px;
+}
+.table-header-container {
+  display: flex;
+  justify-content: center;
+}
+.table-header {
+  width: 835px !important;
+  font-size: 4rem !important;
+}
+.rank-left {
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+}
+
+.rank-number,
+.rank-right {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.table-rank {
+  display: flex;
+}
+.table-title {
+  font-size: 4rem;
+}
+.table-author {
+  font-size: 3rem;
 }
 </style>
