50e3cbbb61c059f42d5a6d46ea8b0a5eae722f6d
김승환 <prpn97@gmail.com>
2023-10-07 18:30:01 +0900

    test: vuetify가 다른 css보다 우선순위 높은 이슈, 순서변경

--- src/main.js
+++ src/main.js
@@ -18,9 +18,10 @@ app.config.globalProperties.$axios = axios.create({
 
 loadFonts();
 app.use(SocketPlugin);
-app.use(vuetify);
+
 app.use(router);
 app.use(store);
+app.use(vuetify);
 // app.use(LightBootstrap);
 
 app.mount("#app");
